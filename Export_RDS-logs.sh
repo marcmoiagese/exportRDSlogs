b0d6fbd0dd426bfe5d190bcefc1b1c2e8143d284
Marc Moya <marc.moya@global.ntt>
2022-02-15 16:43:48 +0100

    uploading script

---
+++ Export_RDS-logs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/.local/bin:/bin/python"
+
+DESTI="path_to_store_logs"
+INSTANCE="instancia"
+
+if [ -n "$1" ];
+then
+        LOGFILE="postgresql.log."$1;
+        CURDAT=$(date "+%Y-%m-%d")
+fi
+if [ -n "$2" ];
+then
+        LOGFILE2="postgresql.log.";
+fi
+
+if [ -z "${LOGFILE}" ];
+then
+  # List avialable logs on AWS
+  /bin/python /root/.local/bin/aws rds describe-db-log-files --db-instance-identifier ${INSTANCE};
+elif [ -z "${LOGFILE2}" ];
+then
+  #Downloading the specified log
+  /root/.local/bin/aws rds download-db-log-file-portion --db-instance-identifier ${INSTANCE} --starting-token 0 --output text --log-file-name "error/"$LOGFILE > $DESTI$LOGFILE;
+ chown custuser: $DESTI$LOGFILE
+elif [ -n "${LOGFILE}" ] && [ -n "${LOGFILE2}" ];
+then
+  #Downloading rank of logs
+  mkdir "$DESTI$CURDAT"
+  for i in $(/root/.local/bin/aws rds describe-db-log-files --db-instance-identifier ${INSTANCE} --profile quantum --output text | awk '{print $3}') ; 
+  do
+        NETEJAT=${i##*.};       # Removing all data except data
+        NET=${NETEJAT::-3};     # Removing time and last -
+
+        if [ "$(date -d $NET +"%s")" -ge "$(date -d ${1::-3} +'%s')" ] && [ "$(date -d $NET +"%s")" -le "$(date -d ${2::-3} +'%s')" ];
+        then
+                /root/.local/bin/aws rds download-db-log-file-portion --db-instance-identifier ${INSTANCE} --starting-token 0 --output text --log-file-name $i > $DESTI$CURDAT"/"$LOGFILE2"-"$NETEJAT;
+        fi
+        chown custuser: "$DESTI$CURDAT" -R
+  done  
+fi
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # exportRDSlogs
 script to allow customer download RDS logs
+
+
+## To review the available logs
+```
+# ./Export_RDS-logs.sh 
+
+{
+    "DescribeDBLogFiles": [
+        {
+            "LastWritten": 1644681429208, 
+            "LogFileName": "error/postgresql.log.2022-02-12-15", 
+            "Size": 4340
+        }, 
+        {
+            "LastWritten": 1644685032429, 
+            "LogFileName": "error/postgresql.log.2022-02-12-16", 
+            "Size": 3909
+        }, 
+        {
+            "LastWritten": 1644688636273, 
+            "LogFileName": "error/postgresql.log.2022-02-12-17", 
+            "Size": 3912
+        } 
+    ]
+}
+```
+
+
+### Download logs
+You can download one log specifying the date
+
+```
+Export_RDS-logs.sh 2022-02-12-15
+```
+
+Or you can download a batch of logs if you define a period of dates
+
+```
+Export_RDS-logs.sh 2022-02-12-15 2022-02-12-17
+```
+
+
+you need set the instance and the path to store the logs on the follow variables
+
+DESTI="path_to_store_logs"
+INSTANCE="instancia"
+
